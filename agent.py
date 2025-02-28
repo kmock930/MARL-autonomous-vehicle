f9e6cb867306055a750e2dfbc85105eb8edd97f4
Kelvin Mock <kelvin.mock@gmail.com>
2025-02-28 00:44:28 -0500

    Added Action Space, Reward and basic Agent structure

---
+++ agent.py
@@ -0,0 +1,54 @@
+import numpy as np
+import sys
+sys.path.append('gym-simplegrid')
+import gym_simplegrid # directory in git
+import gymnasium as gym
+from constants import ACTION_SPACE
+
+class Agent:
+    role: str # Role of the agent
+    position: tuple[int, int] # Position of the agent
+    action_space: tuple[int, int] # Action space of the agent
+
+    def __init__(self, role: str, position: tuple[int, int]):
+        self.role = role
+        self.position = position
+        self.action_space = ACTION_SPACE
+    
+    def act(self, observation: np.ndarray) -> tuple[int, int]:
+        """_summary_
+
+        Args:
+            observation (np.ndarray): partial obersevation of the environment
+
+        Returns:
+            tuple[int, int]: an action to be taken by the agent
+        """
+        # TODO: Not Implemented
+        if (self.role == "leader"):
+            self.speak()
+        else:
+            self.listen()
+        return ACTION_SPACE[8] # Stay
+    
+    def listen(self, message: dict) -> None:
+        """_summary_
+
+        Args:
+            message (dict): message from the leader agent
+        """
+        if (self.role == "follower"):
+            NotImplementedError("Follower agent does not listen")
+        else:
+            ValueError("Leader cannot listen")
+    
+    def speak(self) -> dict:
+        """_summary_
+
+        Returns:
+            dict: message to be sent to the follower agent
+        """
+        if (self.role == "leader"):
+            NotImplementedError("Leader agent does not speak")
+        else:
+            ValueError("Follower cannot speak")
---
+++ constants.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class ACTION_SPACE(Enum):
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
+    UP_LEFT = (-1, -1)
+    UP_RIGHT = (-1, 1)
+    DOWN_LEFT = (1, -1)
+    DOWN_RIGHT = (1, 1)
+    STAY = (0, 0)
+
+class REWARDS(Enum):
+    SOFT_OBSTACLE = -10
+    HARD_OBSTACLE = WALL = -50
+    TARGET = 50
+    STEP = -1
+    CRASH = -50
+    STAY = -3
