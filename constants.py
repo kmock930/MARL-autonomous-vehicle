dae1b6713aead7d0e9a487a576c98c6cb12b4db0
Kelvin Mock <kelvin.mock@gmail.com>
2025-03-17 00:10:45 -0400

    Stablized Environment Definition with debugging stack trace

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 from enum import Enum
 
 class ACTION_SPACE(Enum):
-    UP = (0, -1)
-    DOWN = (0, 1)
-    LEFT = (-1, 0)
-    RIGHT = (1, 0)
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
     UP_LEFT = (-1, -1)
-    UP_RIGHT = (1, -1)
-    DOWN_LEFT = (-1, 1)
+    UP_RIGHT = (-1, 1)
+    DOWN_LEFT = (1, -1)
     DOWN_RIGHT = (1, 1)
     STAY = (0, 0)
 
