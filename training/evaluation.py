4f74bead2a2df5d6bdd021f6c08525cbe517deeb
Kelvin Mock <kelvinmock@cmail.carleton.ca>
2025-04-03 15:23:31 -0400

    Updated Evaluation Script to capture CPU Memory Usages in each training process

--- training/evaluation.py
+++ training/evaluation.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 TRAINING_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+from google.protobuf.json_format import MessageToJson
+import datetime
+from tensorflow.python.profiler import profiler_v2
 
 # Load the saved logs
 logs_df = pd.read_csv(os.path.join(TRAINING_PATH, "logs/evaluation_metrics.csv"))
@@ -42,3 +45,35 @@ if __name__ == "__main__":
                 isSaved=True,
                 isShow=False
             )
+
+    # Load Physical Memory Usage
+    searchString = "Current process memory usage:"
+    LOGS_PATH = os.path.join(TRAINING_PATH, "logs", "train.log")
+    memory_usages = [] # per training process
+    with open(LOGS_PATH, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            for idx, line_content in enumerate(lines):
+                if searchString in line_content:
+                    # Parsing the memory usage number
+                    line_content = line_content.replace(searchString, "")
+                    line_content = line_content.replace("MB", "")
+                    line_content = line_content.strip()
+                    memory_usage = float(line_content)
+                    # Print the memory usage
+                    memory_usages.append(memory_usage)
+                    
+    # Plot Memory Usage
+    plt.plot(memory_usages)
+    plt.title("CPU Memory Usage over Training Process")
+    plt.xlabel("Training Process")
+    plt.ylabel("Memory Usage (MB)")
+    plt.legend()
+    output_dir = os.path.join(TRAINING_PATH, "Plots")
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, "Memory_Usage_over_Training_Process.png")
+    plt.savefig(output_path)
+    print(f"CPU Memory Plot has been saved to {output_path}")
+    # Average Memory Usage
+    avg_memory_usage = sum(memory_usages) / len(memory_usages)
+    print(f"Average CPU Memory Usage: {avg_memory_usage:.2f} MB")
